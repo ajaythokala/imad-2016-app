00eb8bbf6f059ded613fb6a63271390d218917ed
Ajay T <ajaythokala.r@gmail.com>
2016-09-30 21:31:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ element.innerHTML ='New value';
 var img = document.getElementById('madi');
 var maarginLeft =0;
 function moveRight (){
-    marginLeft = marginLeft +1;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
