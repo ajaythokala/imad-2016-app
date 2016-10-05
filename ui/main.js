a5834b4d2cb03626dba3a9eb46e37dafe59e3335
Ajay T <ajaythokala.r@gmail.com>
2016-10-05 17:14:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,11 +15,12 @@ button.onclick=function(){
   request.open('GET','http://ajaythokala.imad.hasura-app.io/counter',true);
   request.send(null);
 };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
-  var request = new XMLHttpRequest(){
+  var request = new XMLHttpRequest();
+  request.onreadystatechange=function(){
+    if(request.readyState ===XMLHttpRequest.DONE){  
       if (request.status === 200){
         var names = request.responseText;
         names=JSON.parse(names);
@@ -31,9 +32,13 @@ submit.onclick = function(){
         ul.innerHTML = list;
   
         }
-          
     }
-};
+      
+  };
+          
+    
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://ajaythokala.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
